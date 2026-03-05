b7747ac296a601f26845058b2cd647ed3c60690d
vivek <vs2299043@gmail.com>
2026-03-05 13:12:45 +0530

    day 5/03/2026 basic setup installation

---
+++ src/component/Admin/index.jsx
@@ -0,0 +1,10 @@
+const Layout = () => {
+    return (
+        <>
+            <h1 className='text-lg font-bold bg-gray-200'>
+                jai shree ram
+            </h1>
+        </>
+    )
+}
+export default Layout;
