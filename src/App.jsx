5534def3c426f60304a3b67b11ca86c1a9f860ab
vivek <vs2299043@gmail.com>
2026-03-08 11:32:34 +0530

    day 08/03/2026 adding multiple pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import 'remixicon/fonts/remixicon.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './component/Admin/index.jsx'
+import Product from './component/Admin/Product.jsx'
+import Order from './component/Admin/Order.jsx'
+import Dashboard from './component/Admin/Dashboard.jsx'
+import Setting from './component/Admin/Setting.jsx'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />} />
+        <Route path='/admin'>
+          <Route path='Product' element={<Product />} />
+          <Route path='Order' element={<Order />} />
+          <Route path='Dashboard' element={<Dashboard />} />
+          <Route path='Setting' element={<Setting />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
